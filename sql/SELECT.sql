7648d9f5eb2b04f4ad3879bec00836673440e357
Eugenia <91629407+Eugenia821@users.noreply.github.com>
2021-10-12 20:21:02 +0300

    Update SELECT.sql

--- sql/SELECT.sql
+++ sql/SELECT.sql
@@ -5,6 +5,6 @@ SELECT [product_id],
        [model_year],
        [list_price]
 FROM [production].[products]
-WHERE product_name LIKE 'Sun%' OR product_name LIKE '%Girl%'
+WHERE [product_name] IN ('Sun%', '%Girl%')
       AND [list_price] <= 1000
       AND [category_id] IN (3, 6)
