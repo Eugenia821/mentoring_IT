074cfda4d1c0a50bcf96d63e13e19a1af111c243
Eugenia <91629407+Eugenia821@users.noreply.github.com>
2021-10-13 00:49:59 +0300

    Update Aggregations.sql

--- sql/Aggregations.sql
+++ sql/Aggregations.sql
@@ -1,1 +1,7 @@
-
+-- for year 2019 I've got other average price than in PPT
+SELECT model_year,
+       AVG([list_price]) AS avg_list_price
+FROM [production].[products]
+WHERE [list_price] >= 832.99
+GROUP BY model_year
+HAVING MIN([list_price]) >= 850
